089828e19d1fbbdcb20b49f01069c4bcfac59896
Anugrah Raghavan <anugrahraghavan912@gmail.com>
2026-08-21 00:19:24 +0530

    test script

--- tests/test_bls.py
+++ tests/test_bls.py
@@ -1,4 +1,3 @@
-print("IT WORKS")
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -6,6 +5,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import lightkurve as lk
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Replace 'your_module_name' with the actual name of your Python file (without the .py)
 from astro.algorithms import box_least_squares_search
+
+print("Downloading Kepler-10 light curve...")
+# Download Kepler-10 data
+lc = lk.search_lightcurve("Kepler-10", mission="Kepler", cadence="long").download_all().stitch()
+
+print("Running BLS search...")
+results = box_least_squares_search(lc)
+
+print("BLS Results found!")
+print(results)
